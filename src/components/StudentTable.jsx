1b4525ce165ccd7546077d7161e70ff83f057df9
Rishabh Sharma <rishabhsharma.7018@gmail.com>
2025-03-19 17:49:14 +0530

    added table using json

--- src/components/StudentTable.jsx
+++ src/components/StudentTable.jsx
@@ -1,35 +1,50 @@
-import React from 'react';
+import React, { useEffect ,useState} from 'react';
 import { useNavigate } from 'react-router-dom';
+
 
 const StudentTable = () => {
     const navToEdit = useNavigate()
     const navToView = useNavigate()
-
     const navigate = useNavigate()
+    const [students, setStudents] = useState([])
+    useEffect(() => {
+        fetch("http://localhost:8000/students").then((res) => {
+            res.json().then((data) => { setStudents(data);
+                // console.log(setStudents(data));
+                // console.log(students);
+             }).catch((err) => { console.log(err); })
+        })
+    }, [])
     return (
         <>
             <div className='bg-[#e5f5ea]'>
                 <div className="container w-11/12 mx-auto">
                     <h1 className='text-6xl text-center font-bold text-[#6A9C89] cursor-pointer'>Student Table </h1>
 
-                    <button className='bg-[#6A9C89] px-6 py-3 rounded-4xl hover:bg-[#C1D8C3] cursor-pointer' onClick={() => { navigate("/student/create/:id") }}>Add Student</button>
+                    <button className='bg-[#6A9C89] px-6 py-3 rounded-4xl hover:bg-[#C1D8C3] cursor-pointer' onClick={() => { navigate("/student/create/:id") }}>Add Student +</button>
 
-                    <table className='w-full text-0.5xl border-4 border-[black] text-center '>
+                    <table className='mt-6 w-full text-0.5xl text-center '>
                         <thead className='text-0.5xl bg-[#FFA725]' >
-                            <tr >
+                          <tr >
                                 <th >Student ID</th>
                                 <th >Student Name</th>
                                 <th >Student Phone</th>
                                 <th >Actions</th>
                             </tr>
                         </thead>
+
                         <tbody className='mt-6'>
-                            <tr>
-                                <td>1</td>
-                                <td>Rishabh</td>
-                                <td>454545454</td>
-                                <td>  <button className='w-20 bg-red-700 px-2 py-2 rounded-2xl hover:bg-red-900 cursor-pointer' onClick={() => { navToEdit("/student/edit/:id") }}>Edit</button> <button className=' w-20 bg-[#6A9C89] px-2 py-2 rounded-2xl hover:bg-[#C1D8C3] cursor-pointer' onClick={() => { navToEdit("/student/view/:id") }}>View</button> <button className='w-20 bg-red-700 px-2 py-2 rounded-2xl hover:bg-red-900 cursor-pointer' onClick={() => { navToEdit("/student/edit/:id") }}>Delete</button></td>
-                            </tr>
+                          { students && students.map((item)=>{
+                            console.log(students);
+                            return(
+                             <tr key={item.id}>
+                             <td>{item.id}</td>
+                             <td>{item.name}</td>
+                             <td>{item.phone}</td>
+                             <td>  <button className='w-20 bg-red-700 px-2 py-2 rounded-2xl hover:bg-red-900 cursor-pointer' onClick={() => { navToEdit("/student/edit/:id") }}>Edit</button> <button className=' w-20 bg-[#6A9C89] px-2 py-2 rounded-2xl hover:bg-[#C1D8C3] cursor-pointer' onClick={() => { navToView("/student/view/:id") }}>View</button> <button className='w-20 bg-red-700 px-2 py-2 rounded-2xl hover:bg-red-900 cursor-pointer' onClick={() => { navToEdit("/student/edit/:id") }}>Delete</button></td>
+                         </tr>)
+
+                          })} 
                         </tbody>
                     </table>
                 </div>
